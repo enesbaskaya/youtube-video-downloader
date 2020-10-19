1f802bc3ac00e1b02cd72e75f89c5eaccf322e6e
Charitra Agarwal <charitraagarwal1022@gmail.com>
2020-10-19 10:43:54 -0700

    Added minor changes to the existing code for enhanced functionality

--- downloader.py
+++ downloader.py
@@ -2,8 +2,47 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 import sys
 from pytube import YouTube
+import datetime
+from threading import Thread
+import os
+import logging
 
 __author__ = "enesbaskaya"
+
+
+# some global values
+windowIcon = "eb.png"
+videoURL = ''
+videoMetadata = []
+video_dir = os.path.expanduser("~") + '\\Downloads\\YouTube Video Downloader\\' # get the absolute path of destination folder
+
+
+
+def get_video(url):
+    """get the video from the url and return the data"""
+    # video object
+    yt = YouTube(url)
+    # video metadata
+    title = yt.title
+    duration = str(datetime.timedelta(seconds = yt.length))
+    views = ("{:,}".format(yt.views))
+    videoMetadata.append(title)
+    videoMetadata.append(duration)
+    videoMetadata.append(views)
+
+
+
+def download_video():
+    """Download the desired video in the specified directory"""
+    try:
+        try: os.mkdir(video_dir)          # create a temporary folder for PyMediaPlayer if not present
+        except Exception as e: pass     # generates error if already present
+    
+        yt = YouTube(videoURL)
+        ys = yt.streams.get_highest_resolution()
+        #ys.download(video_dir)
+    except Exception as e: print("Error:", e)
+    
 
 
 class Window(QMainWindow):
@@ -46,25 +85,76 @@ class Window(QMainWindow):
     def settings(self):
         # pencere başlık ve icon
         windowTitle = "Youtube DownLoader"
-        windowIcon = "eb.png"
         self.setWindowTitle(windowTitle)
         self.setWindowIcon(QIcon(windowIcon))
 
         # pencere boyutlandırma
         self.setGeometry(250, 250, 400, 80)
 
+
     # indirme butonu aksiyon
     def download(self):
-        title = "Download Manager"
-        contentText = "Download Complete!"
-        url = self.link.text()
+        global videoURL
+        url = videoURL = self.link.text()
+        
+        try:
+            # start video searching in a new thread
+            t = Thread(target=get_video, args =(url,))
+            t.start()
+            if t.is_alive():print('Processing')
+            else: print('An Error occured')
+            t.join()
 
-        # video kalilesi
-        res = "1080p"
-        YouTube(url).streams.filter(res=res).first().download()
-        if (YouTube):
-            QMessageBox.information(self, title, contentText)
+            # get the metadata of the video
+            title, duration, views = videoMetadata
 
+
+            # confirmation message box details
+            conf_box = QMessageBox()
+            conf_box.setIcon(QMessageBox.Information)
+            conf_box.setText("Do you want to download this video?")
+            conf_box.setInformativeText("Title: {0}\nDuration: {1}\nViews: {2}".format(title,duration,views))
+            conf_box.setWindowTitle("YouTube Video Downloader")
+            conf_box.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+            conf_box.setWindowIcon(QIcon(windowIcon))
+            conf_box.buttonClicked.connect(self.confirm_download)
+            conf_box.exec_()
+            
+        except Exception as e: print(e)
+
+
+
+    # Start downlaod only if confirmed
+    def confirm_download(self, status):
+        """Download only if confirmed"""
+        if status.text().lower() == "ok":
+            # start video downloading in a new thread
+            d = Thread(target=download_video)
+            d.start()
+            
+            if d.is_alive(): print('Downloading')
+            else: print('An Error occured')
+            d.join()
+            
+            # After its downloaded
+            contentText = "Download Complete!"
+            alert_box = QMessageBox()
+            alert_box.setIcon(QMessageBox.Information)
+            alert_box.setTitle(contentText)
+            alert_box.setInformativeText("Downloaded at: ", video_dir)
+            alert_box.setWindowTitle("YouTube Video Downloader")
+            alert_box.setStandardButtons(QMessageBox.Ok)
+            alert_box.setWindowIcon(QIcon(windowIcon))
+            alert_box.exec_()
+
+        
+        print(videoMetadata)
+        videoMetadata.clear()
+        print(videoMetadata)
+
+        
+            
+    
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
